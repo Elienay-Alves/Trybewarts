0bb86d909d55ed7c46edf4132082c96f7e51a7cb
Elienay-Alves <elienay8486@gmail.com>
2022-01-11 16:22:56 -0300

    Colocamos os elementos html do Requisito 5

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const email = document.getElementById('email');
-const senha = document.getElementById('senha');
-const botao = document.getElementById('botao');
+const password = document.getElementById('senha');
+const button = document.getElementById('botao');
 
 // Vamos criar uma função para validar o e-mail no avaliador. Essa função vai ver se o valor dentro dos inputs é o exigido pelo avaliador e caso seja irá emitir um alerta de bem vindo ao clicarmos em enviar, caso não ele mandará outro indicando um err o:
 
 function validation() {
-  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
   }
 }
 
-botao.addEventListener('click', validation);
+button.addEventListener('click', validation);
