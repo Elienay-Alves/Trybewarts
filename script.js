6d8e99260b7019b995bcfa707fe2103b39d17d3d
Elienay-Alves <elienay8486@gmail.com>
2022-01-11 18:41:58 -0300

    Criamos o botao porem ele esta iniciando habilitado

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const email = document.getElementById('email');
 const password = document.getElementById('senha');
 const button = document.getElementById('botao');
+const sendButton = document.querySelector('#submit-btn');
+const agreementChecked = document.querySelector('#agreement');
 
 // Vamos criar uma função para validar o e-mail no avaliador. Essa função vai ver se o valor dentro dos inputs é o exigido pelo avaliador e caso seja irá emitir um alerta de bem vindo ao clicarmos em enviar, caso não ele mandará outro indicando um err o:
 
@@ -13,3 +15,15 @@ function validation() {
 }
 
 button.addEventListener('click', validation);
+
+// Vamos criar uma função que checa se a pessoa marcou check no agreement ou não. Caso ela tenha marcado ela podera enviar o formulario dela e caso ela não tenha marcado o botão ficara desativado:
+
+function areYouareRobbot() {
+  if (agreementChecked.checked) {
+    sendButton.disabled = false;
+  } else {
+    sendButton.disabled = true;
+  }
+}
+
+agreementChecked.addEventListener('click', areYouareRobbot);
