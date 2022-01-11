1b691279f5b74f1698650053c55ed85817a2f156
Elienay-Alves <elienay8486@gmail.com>
2022-01-11 16:04:35 -0300

    #

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const email = document.getElementById('email');
 const senha = document.getElementById('senha');
 const botao = document.getElementById('botao');
+
+// Vamos criar uma função para validar o e-mail no avaliador:
+function validation() {
+
+}
