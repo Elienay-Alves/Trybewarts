e001974945008cbc87f60b5df557a7c3813679bc
Elienay-Alves <elienay8486@gmail.com>
2022-01-11 19:16:56 -0300

    Finalizamos o requisito 20. Quase 100%

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const password = document.getElementById('senha');
 const button = document.getElementById('botao');
 const sendButton = document.querySelector('#submit-btn');
 const agreementChecked = document.querySelector('#agreement');
+const textArea = document.querySelector('#textarea');
+const remainingLetters = document.querySelector('#counter');
+const maxLetters = 500;
 
 // Vamos criar uma função para validar o e-mail no avaliador. Essa função vai ver se o valor dentro dos inputs é o exigido pelo avaliador e caso seja irá emitir um alerta de bem vindo ao clicarmos em enviar, caso não ele mandará outro indicando um err o:
 
@@ -18,7 +21,7 @@ button.addEventListener('click', validation);
 
 // Vamos criar uma função que checa se a pessoa marcou check no agreement ou não. Caso ela tenha marcado ela podera enviar o formulario dela e caso ela não tenha marcado o botão ficara desativado:
 
-function areYouareRobbot() {
+function areYouAreRobbot() {
   if (agreementChecked.checked) {
     sendButton.disabled = false;
   } else {
@@ -26,4 +29,14 @@ function areYouareRobbot() {
   }
 }
 
-agreementChecked.addEventListener('click', areYouareRobbot);
+agreementChecked.addEventListener('click', areYouAreRobbot);
+
+// Vamos criar uma função que vai mostrar o numero de letras que falta:
+
+function writeWhileYouCan() {
+  const toTheEnd = maxLetters - textArea.value.length;
+
+  remainingLetters.textContent = `${toTheEnd} characters remaining`;
+}
+
+textArea.addEventListener('input', writeWhileYouCan);
