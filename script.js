e9b28368a534dba105c7d8c29a0c4b06820ac891
Elienay-Alves <elienay8486@gmail.com>
2022-01-11 16:15:51 -0300

    Completamos o requisito 4

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const email = document.getElementById('email');
 const senha = document.getElementById('senha');
 const botao = document.getElementById('botao');
 
-// Vamos criar uma função para validar o e-mail no avaliador. Essa função vai ver se o valor dentro dos inputs é o exigido pelo avaliador e caso seja irá emitir um alerta de bem vindo ao clicarmos em enviar, caso não ele mandará outro indicando um erro:
+// Vamos criar uma função para validar o e-mail no avaliador. Essa função vai ver se o valor dentro dos inputs é o exigido pelo avaliador e caso seja irá emitir um alerta de bem vindo ao clicarmos em enviar, caso não ele mandará outro indicando um err o:
 
 function validation() {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
