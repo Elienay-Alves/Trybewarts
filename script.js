cf683ced767f91bf05380a9af53db8b8106a41a5
Elienay-Alves <elienay8486@gmail.com>
2022-01-11 16:10:41 -0300

    Criamos a validaçao do nosso login

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ const email = document.getElementById('email');
 const senha = document.getElementById('senha');
 const botao = document.getElementById('botao');
 
-// Vamos criar uma função para validar o e-mail no avaliador:
-function validation() {
+// Vamos criar uma função para validar o e-mail no avaliador. Essa função vai ver se o valor dentro dos inputs é o exigido pelo avaliador e caso seja irá emitir um alerta de bem vindo ao clicarmos em enviar, caso não ele mandará outro indicando um erro:
 
+function validation() {
+  if (email.value === 'tryber@tester.com' && senha.value === '123456') {
+    alert('Olá, tryber!');
+  } else {
+    alert('Email ou senha incorretos.');
+  }
 }
+
+botao.addEventListener('click', validation);
