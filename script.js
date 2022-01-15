744a1d4f52df8a88bd01efb198c8605ff6e4a75e
Elienay-Alves <elienay8486@gmail.com>
2022-01-15 09:30:31 -0300

    cria a funçao que salva o nome

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const agreementChecked = document.querySelector('#agreement');
 const textArea = document.querySelector('#textarea');
 const remainingLetters = document.querySelector('#counter');
 const maxLetters = 500;
+const takingTheSubjects = document.querySelector('.subject');
 
 // Vamos criar uma função para validar o e-mail no avaliador. Essa função vai ver se o valor dentro dos inputs é o exigido pelo avaliador e caso seja irá emitir um alerta de bem vindo ao clicarmos em enviar, caso não ele mandará outro indicando um err o:
 
@@ -18,9 +19,9 @@ const validation = () => {
 
 button.addEventListener('click', validation);
 
-// Vamos criar uma função que checa se a pessoa marcou check no agreement ou não. Caso ela tenha marcado ela podera enviar o formulario dela e caso ela não tenha marcado o botão ficara desativado:
+// Vamos criar uma função que checa se a pessoa marcou check no agreement ou não. Caso ela tenha marcado ela podera enviar o formulario dela e caso ela não tenha marcado o botão ficará desativado:
 
-const areYouAreRobbot = () => {
+const canYouAgrePlease = () => {
   if (agreementChecked.checked) {
     sendButton.disabled = false;
   } else {
@@ -28,14 +29,28 @@ const areYouAreRobbot = () => {
   }
 };
 
-agreementChecked.addEventListener('click', areYouAreRobbot);
+agreementChecked.addEventListener('click', canYouAgrePlease);
 
-// Vamos criar uma função que vai mostrar o numero de letras que falta:
+// Vamos criar uma função que vai mostrar o número de letras que falta:
 
-function writeWhileYouCan() {
+function itsOverBrow() {
   const toTheEnd = maxLetters - textArea.value.length;
 
   remainingLetters.textContent = `${toTheEnd} characters remaining`;
 }
 
-textArea.addEventListener('input', writeWhileYouCan);
+textArea.addEventListener('input', itsOverBrow);
+
+// Vamos criar as funções que pegam os valores dos inputs:
+
+// Começando pela função que pega o nome:
+
+const fullNamaeForm = () => {
+  const name = document.getElementById('input-name').value;
+  const lastName = document.getElementById('input-lastname').value;
+  const fullName = document.getElementById('fullname');
+  fullName.innerHTML = '';
+  const createP = document.createElement('p');
+  fullName.appendChild(createP);
+  createP.innerText = `Nome: ${name} ${lastName}`;
+};
