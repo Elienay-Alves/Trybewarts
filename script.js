9ef3cc13fd53b6553554d402d583032432ac953e
Elienay-Alves <elienay8486@gmail.com>
2022-01-15 08:35:59 -0300

    Funçoes refatoradas

--- script.js
+++ script.js
@@ -9,25 +9,24 @@ const maxLetters = 500;
 
 // Vamos criar uma função para validar o e-mail no avaliador. Essa função vai ver se o valor dentro dos inputs é o exigido pelo avaliador e caso seja irá emitir um alerta de bem vindo ao clicarmos em enviar, caso não ele mandará outro indicando um err o:
 
-function validation() {
+const validation = () => {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
-  } else {
-    alert('Email ou senha inválidos.');
   }
-}
+  alert('Email ou senha inválidos.');
+};
 
 button.addEventListener('click', validation);
 
 // Vamos criar uma função que checa se a pessoa marcou check no agreement ou não. Caso ela tenha marcado ela podera enviar o formulario dela e caso ela não tenha marcado o botão ficara desativado:
 
-function areYouAreRobbot() {
+const areYouAreRobbot = () => {
   if (agreementChecked.checked) {
     sendButton.disabled = false;
   } else {
     sendButton.disabled = true;
   }
-}
+};
 
 agreementChecked.addEventListener('click', areYouAreRobbot);
 
